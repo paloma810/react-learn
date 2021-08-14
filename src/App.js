e696336c6b1262730e9f60b181896b5e2a1756ed
paloma810 <mcyk49129@gmail.com>
2021-08-14 22:31:00 +0900

    fix App.js to learn Components.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 import React from 'react';
 
+// JSXはHTMLタグライク中たちでわかりやすく表現できる言語の一つ？
+
+// HTMLタグのlabelは input タグの ラベルとして作用する（inputのid属性とlabelのforが連動するイメージ)
+// 例えば、チェックボックスのlabelを定義することで、文字をクリックすることでチェック可能となる
+
+// JSXはそのままでは実行できない --> トランスパイリングによって、Javascriptの形式に変換され実行可能となる
+// babelがトランスパイリングしてくれている
+// REPL で確認できる cf) https://babeljs.io/repl/
+
+// webpackが、複数の.jsファイルをbundleしてくれる cf)https://webpack.js.org/
+
+// 2. functionnal component, 1. class componentの2種類のコンポーネントがある
+//    1. class App extends Component { render () { return {} } } ※ Componentをimportする必要がある
+//    2. const App = () => {}
+
 function App() {
   return (
     <React.Fragment>
